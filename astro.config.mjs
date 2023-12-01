cdad17a65653d4973137f05fd722bd9afaa8f4c4
adamibnu <adamibnu157@gmail.com>
2023-12-01 12:42:54 +0700

    enhance(server): setup serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
   site: 'https://adamibnu.com',
   integrations: [tailwind(), sitemap(), ],
   styles: [
