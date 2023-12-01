109cf4c603f049673d5cb17f212ea63cb1bda048
adamibnu <adamibnu157@gmail.com>
2023-12-01 11:24:47 +0700

    core(map): setup sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+
 export default defineConfig({
   site: 'https://adamibnu.com',
   integrations: [tailwind(), sitemap(), ],
