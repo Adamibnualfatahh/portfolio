e689221401c1a442affada532a0bb931a2e518e6
adamibnu <adamibnu157@gmail.com>
2026-01-03 09:51:23 +0700

    feat: Integrate Astro Partytown to offload Google Analytics scripts.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,17 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel";
 
+import partytown from '@astrojs/partytown';
+
 export default defineConfig({
   output: 'static',
   adapter: vercel(),
   site: 'https://www.adamibnu.dev',
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), partytown({
+    config: {
+      forward: ['dataLayer.push'],
+    },
+  })],
   styles: [
     '@fortawesome/fontawesome-free/css/all.min.css'
   ],
