4366d98d8cba624cf0386eb448e06ee08e4fb1f5
adamibnu <adamibnu157@gmail.com>
2026-09-12 05:34:51 +0700

    Align sitemap with canonical domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import partytown from '@astrojs/partytown';
 export default defineConfig({
   output: 'static',
   adapter: vercel(),
-  site: 'https://adamibnu.my.id',
+  site: 'https://www.adamibnu.my.id',
   integrations: [tailwind(), sitemap(), partytown({
     config: {
       forward: ['dataLayer.push'],
